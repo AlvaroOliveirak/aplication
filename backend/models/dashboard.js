b0ba78f91d334d88e52a9df6ef01b5a703434a99
Alvaro oliveira <alvarooliver1802@gmail.com>
2026-07-06 20:33:39 -0300

    refactor

--- backend/models/dashboard.js
+++
@@ -1,8 +0,0 @@
-import { sequelize } from "./index.js";
-import { DataTypes } from "sequelize";
-
-export const Dashboard = sequelize.define("dashboard", {
-  name: DataTypes.STRING,
-  query: DataTypes.TEXT,
-  metricId: DataTypes.STRING
-});
--- backend/models/index.js
+++
@@ -1,7 +0,0 @@
-import Sequelize from "sequelize";
-
-export const sequelize = new Sequelize("userdb", "postgres", "alvarooliver18", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5433
-});
